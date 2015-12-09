69ad8032b20a100354039281cbc4bdeec5cdda0d
Nathan F Alison <nathan.f.alison@gmail.com>
2015-12-08 17:45:35 -0800

    Add IAction interface and a sample implementation.

---
+++ Assets/Scripts/ChangeTerrainAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTerrainAction : MonoBehaviour, IAction
+{
+	public void Act(Coordinate target)
+	{
+		// TODO change, not destroy, the target.
+		Debug.LogFormat("Acting on coordinate {0}.", target);
+		Destroy(GameManager.gm.terrain.TerrainGrid[target].gameObject);
+		GameManager.gm.terrain.TerrainGrid.Remove(target);
+	}
+}
---
+++ Assets/Scripts/IAction.cs
@@ -0,0 +1,6 @@
+using System;
+
+public interface IAction
+{
+	void Act(Coordinate coordinate);
+}
