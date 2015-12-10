adf80474ecd66f1a2bd62a4221f35bf530196f8f
Nathan F Alison <nathan.f.alison@gmail.com>
2015-12-10 15:39:22 -0800

    Add a ResourcePile prefab and script.
    This resource pile just contains a list of resources.

---
+++ Assets/Scripts/ResourcePile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ResourcePile : MonoBehaviour
+{
+	public ResourceCount[] resources;
+
+	void OnMouseOver()
+	{
+		Debug.Log("Going over resources.");
+	}
+}
+
+[Serializable]
+public class ResourceCount
+{
+	public ResourceType type;
+	public int count;
+}
+
+public enum ResourceType { Red, Yellow, Green, Blue }
