93c2e04cae8e38057e1d7bae4d6ed93a6c774da6
Nathan F Alison <nathan.f.alison@gmail.com>
2015-12-13 15:06:22 -0800

    Add a string display for picking up resources.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -69,6 +69,11 @@ public class UIManager : MonoBehaviour
 			ability = string.Format("Pick up {0}\n\nPress <Space> to activate.",
 				creature.GetComponent<ChangeTerrainAbility>().carryType);
 		}
+		else if (creature.GetComponent<CarryResourceAbility>() != null)
+		{
+			ability = string.Format("Pick up {0} resources\n\nPress <Space> to activate.",
+				creature.GetComponent<CarryResourceAbility>().capacity);
+		}
 		else
 		{
 			ability = "None";
