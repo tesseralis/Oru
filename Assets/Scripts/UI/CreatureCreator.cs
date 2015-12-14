1730db437ed41848c0216acb64a418c98bfc3cd5
Nathan F Alison <nathan.f.alison@gmail.com>
2015-12-14 00:03:19 -0800

    Make improvements to the UI for the creature creator.

--- Assets/Scripts/UI/CreatureCreator.cs
+++ Assets/Scripts/UI/CreatureCreator.cs
@@ -5,27 +5,27 @@ using System.Collections;
 public class CreatureCreator : MonoBehaviour {
 
 	public GameObject createMarker;
+	public GameObject positiveMarker;
+	public GameObject negativeMarker;
 
 	// Handler that is called when the creature is created
 	public event Action<Creature> Created;
 
-	private bool isCreating = false;
 	private CreatureType currentCreatureType;
 
 	public void StartCreation(CreatureType creature)
 	{
-		isCreating = true;
 		currentCreatureType = creature;
-		if (createMarker) { createMarker.SetActive(true); }
-		GameManager.Terrain.MouseEnterBlock += MoveCreateMarker;
+		GameManager.Terrain.MouseEnterBlock += ShowCreateMarker;
+		GameManager.Terrain.MouseExitBlock += HideCreateMarker;
 		GameManager.Terrain.ClickBlock += CreateCreature;
 	}
 
 	public void StopCreation()
 	{
-		isCreating = false;
 		if (createMarker) { createMarker.SetActive(false); }
-		GameManager.Terrain.MouseEnterBlock -= MoveCreateMarker;
+		GameManager.Terrain.MouseEnterBlock -= ShowCreateMarker;
+		GameManager.Terrain.MouseExitBlock -= HideCreateMarker;
 		GameManager.Terrain.ClickBlock -= CreateCreature;
 	}
 
@@ -34,28 +34,42 @@ public class CreatureCreator : MonoBehaviour {
 		if (createMarker) { createMarker.SetActive(false); }
 	}
 
-	void MoveCreateMarker(Coordinate coordinate)
+	void ShowCreateMarker(Coordinate coordinate)
 	{
-		if (createMarker != null && isCreating)
+		if (createMarker)
 		{
+			createMarker.SetActive(true);
 			// Update the position visually
 			GameManager.gm.SetPosition(createMarker, coordinate);
+			if (GameManager.Creatures.CanCreateCreature(currentCreatureType, coordinate))
+			{
+				if (positiveMarker) { positiveMarker.SetActive(true); }
+				if (negativeMarker) { negativeMarker.SetActive(false); }
+			}
+			else
+			{		
+				if (positiveMarker) { positiveMarker.SetActive(false); }
+				if (negativeMarker) { negativeMarker.SetActive(true); }
+			}
 		}
+	}
+
+	void HideCreateMarker(Coordinate coordinate)
+	{
+		if (createMarker) { createMarker.SetActive(false); }
 	}
 
 	void CreateCreature(Coordinate coordinate)
 	{
-		if (createMarker != null && isCreating)
+		if (GameManager.Creatures.CanCreateCreature(currentCreatureType, coordinate))
 		{
-			if (GameManager.Creatures.CanCreateCreature(currentCreatureType, coordinate))
-			{
-				// If everything passes, add the creature to the list of creatures
-				var creature = GameManager.Creatures.CreateCreature(currentCreatureType, coordinate);
-				// We are no longer creating
-				StopCreation();
-
-				if (Created != null) { Created(creature); }
-			}
+			// If everything passes, add the creature to the list of creatures
+			var creature = GameManager.Creatures.CreateCreature(currentCreatureType, coordinate);
+			// We are no longer creating
+			StopCreation();
+		
+			if (Created != null) { Created(creature); }
 		}
+
 	}
 }
