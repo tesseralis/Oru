f01cfb3ad4c3892ade9eabc746a3978f607305cc
Nathan F Alison <nathan.f.alison@gmail.com>
2015-12-16 23:51:46 -0800

    Add a separate "keydown" event.

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -7,22 +7,33 @@ using System.Collections.Generic;
 public class InputController : MonoBehaviour
 {
 	private IDictionary<KeyCode, Action> keyEvents;
+	private IDictionary<KeyCode, Action> keyDownEvents;
 
 	public IDictionary<KeyCode, Action> Key { get { return keyEvents; } }
+	public IDictionary<KeyCode, Action> KeyDown { get { return keyDownEvents; } }
 
 	void Awake ()
 	{
 		keyEvents = new Dictionary<KeyCode, Action>();
+		keyDownEvents = new Dictionary<KeyCode, Action>();
 		// Initialize events we want to use
 		foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
 		{
 			keyEvents[key] = null;
+			keyDownEvents[key] = null;
 		}
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
+		foreach (KeyCode keycode in keyDownEvents.Keys)
+		{
+			if (Input.GetKeyDown(keycode))
+			{
+				if (keyDownEvents[keycode] != null) { keyDownEvents[keycode](); }
+			}
+		}
 		foreach (KeyCode keycode in keyEvents.Keys)
 		{
 			if (Input.GetKey(keycode))
