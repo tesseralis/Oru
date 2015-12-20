d867b70dda12f15c20f67d844754ace645098c79
Nathan F Alison <nathan.f.alison@gmail.com>
2015-12-20 12:15:11 -0800

    Add shortcuts to restart the level and go back to main menu.

--- Assets/Scripts/UI/WinnerPanel.cs
+++ Assets/Scripts/UI/WinnerPanel.cs
@@ -6,6 +6,7 @@ public class WinnerPanel : MonoBehaviour {
 
 	public string menuScene = "Menu";
 
+	// TODO refactor these methods so that they can be used at any point in the level
 	public void ReturnToMenu()
 	{
 		SceneManager.LoadScene(menuScene);
