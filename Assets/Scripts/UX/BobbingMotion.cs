6da3958937e0ee2152b4c9901282bd396fec92fc
Nathan F Alison <nathan.f.alison@gmail.com>
2016-01-16 14:40:54 -0800

    Add bobbing motion script for blueprints and goals.

---
+++ Assets/Scripts/UX/BobbingMotion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Script that causes objects to bob up and down over time.
+public class BobbingMotion : MonoBehaviour
+{
+
+	public float magnitude = 0.0025f;
+	public float period = 3f;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad * period) * magnitude);
+	}
+}
