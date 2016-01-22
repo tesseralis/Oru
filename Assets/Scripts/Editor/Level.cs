466afe32c1c6549e3757bde463be0d5b89ccfa44
Nathan F Alison <nathan.f.alison@gmail.com>
2016-01-22 15:32:27 -0800

    Add a window to be able to load and save levels.

---
+++ Assets/Scripts/Editor/Level.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+public class Level : EditorWindow
+{
+	[MenuItem("Window/Level")]
+	public static void  ShowWindow () {
+		EditorWindow.GetWindow(typeof(Level));
+	}
+
+	public void OnGUI()
+	{
+		GUILayout.Label ("Level Settings", EditorStyles.boldLabel);
+		if (GUILayout.Button("Load"))
+		{
+			LoadLevel();
+		}
+		if (GUILayout.Button("Save"))
+		{
+			SaveLevel();
+		}
+	}
+
+	public void LoadLevel()
+	{
+		// TODO make the deserializer work
+		// TODO be able to load all the level
+		Deserializer.DeserializeLevel("crane");
+		Debug.Log("Level Loaded");
+	}
+
+	public void SaveLevel()
+	{
+		// TODO Save the level
+		Debug.Log("Level Saved");
+	}
+}
