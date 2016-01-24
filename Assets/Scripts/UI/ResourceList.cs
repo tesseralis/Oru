ba65c8a2a7188cb39681ca412e9340157fc70ad3
Nathan F Alison <nathan.f.alison@gmail.com>
2016-01-24 02:44:33 -0800

    Make the resource info smaller so that everything fits.

--- Assets/Scripts/UI/ResourceList.cs
+++ Assets/Scripts/UI/ResourceList.cs
@@ -9,7 +9,6 @@ public class ResourceList : MonoBehaviour
 {
 
 	public GameObject resourceCountPanelPrefab;
-	public float resourceSpacing = 30f;
 
 	// TODO a lot of this can be factored with the methods in RecipeList
 	public void ShowResources(IDictionary<ResourceType, int> recipe)
@@ -17,6 +16,8 @@ public class ResourceList : MonoBehaviour
 		gameObject.SetActive(true);
 		// Delete all the children
 		gameObject.DestroyAllChildren();
+
+		var resourceCountHeight = resourceCountPanelPrefab.GetComponent<RectTransform>().rect.height;
 
 		int i = 0;
 		foreach(var entry in recipe)
@@ -26,7 +27,7 @@ public class ResourceList : MonoBehaviour
 
 			// Translate the new object by the index amount
 			var transform = newResource.GetComponent<RectTransform>();
-			transform.anchoredPosition += Vector2.down * i * resourceSpacing;
+			transform.anchoredPosition += Vector2.down * i * resourceCountHeight;
 			newResource.GetComponent<ResourceCountPanel>().SetContents(entry.Key, entry.Value);
 			i++;
 		}
