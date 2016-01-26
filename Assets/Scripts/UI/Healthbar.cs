92b886eee9ba625522115203da3e3c80ac2309a6
Nathan F Alison <nathan.f.alison@gmail.com>
2016-01-25 23:49:05 -0800

    Change healthcare color based on creature health.

--- Assets/Scripts/UI/Healthbar.cs
+++ Assets/Scripts/UI/Healthbar.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Healthbar : MonoBehaviour
 {
 	public RectTransform currentHealth;
+	public Color healthColor = Color.green;
+	public Color lowHealthColor = Color.red;
 
 	public void SetHealth(int health)
 	{
+		// Set the current health to be a ratio of the full health
 		var ratio = (float) health / CreatureController.maxHealth;
 		var width = gameObject.GetComponent<RectTransform>().rect.width;
 		var offsetRight = (1.0f - ratio) * width;
 		currentHealth.offsetMax = Vector2.left * offsetRight;
+
+		var image = currentHealth.GetComponent<Image>();
+		// Change the color of the health bar
+		image.color = health <= CreatureController.lowHealth ? lowHealthColor : healthColor;
 	}
 }
