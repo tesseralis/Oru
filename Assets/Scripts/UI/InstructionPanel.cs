54fde3ea0ec787a090cfa660d5165835a4b1268c
Nathan F Alison <nathan.f.alison@gmail.com>
2016-01-26 20:43:44 -0800

    Add a panel to the UI that displays instructions.

---
+++ Assets/Scripts/UI/InstructionPanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		// TODO Show or hide instructions based on level text
+	}
+
+	public void HideInstructions()
+	{
+		gameObject.SetActive(false);
+	}
+}
